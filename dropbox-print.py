314a1e81718549f34592b13859d71c520be63118
Asrio <coltinkunz@gmail.com>
2021-11-24 14:24:57 -0500

    Add automatic printing for pdf, jpg, jpeg, and png file types.

--- dropbox-print.py
+++ dropbox-print.py
@@ -1,5 +1,7 @@
+import re
 import os
 import dropbox
+import win32con
 
 from keyboard import press
 from shutil import copyfileobj
@@ -9,6 +11,45 @@ from dotenv import load_dotenv
 from contextlib import closing
 from os import getenv, startfile, remove
 
+from win32gui import (
+    SetForegroundWindow,
+    FindWindow,
+    GetWindowText,
+    EnumWindows,
+    PostMessage,
+)
+
+
+class WindowMgr:
+    """Encapsulates some calls to the winapi for window management"""
+
+    def __init__(self):
+        """Constructor"""
+        self._handle = None
+
+    def find_window(self, class_name, window_name=None):
+        """find a window by its class_name"""
+        self._handle = FindWindow(class_name, window_name)
+
+    def close_window(self):
+        PostMessage(self._handle, win32con.WM_CLOSE, 0, 0)
+
+    def _window_enum_callback(self, hwnd, wildcard):
+        """Pass to win32gui.EnumWindows() to check all the opened windows"""
+        if re.match(wildcard, str(GetWindowText(hwnd))) is not None:
+            self._handle = hwnd
+
+    def find_window_wildcard(self, wildcard):
+        """find a window whose title matches the wildcard regex"""
+        self._handle = None
+        EnumWindows(self._window_enum_callback, wildcard)
+
+    def set_foreground(self):
+        """put the window in the foreground"""
+        SetForegroundWindow(self._handle)
+
+
+w = WindowMgr()
 # Load .env file
 load_dotenv()
 # Connect to Dropbox API
@@ -46,8 +87,21 @@ while True:
                     if os.path.isfile(temp_path):
                         print(f"Printing: {temp_path}")
                         startfile(temp_path, "print")
-                        print(f"Added to print queue: {temp_path}")
+                        file_ending = temp_path.rsplit(".", 1)[1]
+                        # pull up window
+                        hwnd = None
                         sleep(2)
+                        if (
+                            file_ending == "png"
+                            or file_ending == "jpg"
+                            or file_ending == "jpeg"
+                        ):
+                            w.find_window_wildcard(".*Print.*")
+                            w.set_foreground()
+                        elif file_ending == "pdf":
+                            w.find_window_wildcard(".*Acrobat Pro DC.*")
+                            w.close_window()
+                        print(f"Added to print queue: {temp_path}")
                         press("enter")
                         dbx.files_delete(entry.path_lower)
                     else:
